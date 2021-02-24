a99a5385000b6d05a293b5fec72804c2f3300cd2
Luca Vercelli <luca.vercelli.to@gmail.com>
2021-02-24 16:58:49 +0100

    query

--- ws/GeneraDatiTest.php
+++ ws/GeneraDatiTest.php
@@ -33,17 +33,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // REPERIRE DATI DA DB
     $primo = "DATE('$anno-$mese-01')";
-    $query1 = "SELECT DISTINCT matricola_dipendente FROM ore_presenza_lul WHERE data >= $primo AND data <= LAST_DAY($primo) AND ore_presenza > 0";
-    $query2 = "SELECT DISTINCT id_progetto,id_wp FROM progetti_wp WHERE (data_inizio >= $primo AND data_inizio <= LAST_DAY($primo)) OR (data_fine >= $primo AND data_fine <= LAST_DAY($primo))";
-    $query3 = "SELECT DISTINCT data FROM ore_presenza_lul WHERE data >= $primo AND data <= LAST_DAY($primo) AND ore_presenza > 0";
+    $query_matr = "SELECT DISTINCT MATRICOLA_DIPENDENTE from ORE_PRESENZA_LUL " .
+                "WHERE DATA >= $primo AND DATA <= LAST_DAY($primo) AND ORE_PRESENZA > 0";
+    $query_wp = "SELECT ID_PROGETTO, ID_WP, DATA_INIZIO, DATA_FINE, MONTE_ORE_RESIDUO " . 
+                "FROM progetti_wp_residuo ".
+                "WHERE (DATA_INIZIO >= $primo AND DATA_INIZIO <= LAST_DAY($primo)) OR (DATA_FINE >= $primo AND DATA_FINE <= LAST_DAY($primo)) AND MONTE_ORE_RESIDUO > 0";
+    $query_date = "SELECT DISTINCT DATA FROM ORE_PRESENZA_LUL " .
+                "WHERE DATA >= $primo AND DATA <= LAST_DAY($primo) AND ORE_PRESENZA > 0";
     
     // TODO selezionare anche i dati preesistenti
 
-    $matricole = select_column($query1);
-    $wp = select_list($query2);
-    $date = select_column($query3);
+    $matricole = select_column($query_matr);
+    $wp = select_list($query_wp);
+    $date = select_column($query_date);
     
-    // Inizializzo la struttura x
+    
+    // Inizializzo la struttura x (ore lavorate dal dipendente i sul WP j il giorno k)
     $x = array();
     for ($i = 1; $i <= count($matricole); $i++) {
         $x[$i] = array();
@@ -54,8 +59,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
         }
     }
-    // ...
+
+    // Inizializzo la struttura M (monte ore) e la $wp_rsr (progetti-risorse)
+    $M = array();
+    $wp_rsr = array();
+    for ($j = 1; $j <= count($wp); $j++) {
+        $M[$j] = $wp["MONTE_ORE_RESIDUO"];
+        
+        $idprogetto = $wp["ID_PROGETTO"];
+        $idwp = $wp["ID_WP"];
+        $query_wp_rsr = "SELECT MATRICOLA_DIPENDENTE FROM progetti_wp_risorse WHERE ID_PROGETTO='$idprogetto' AND ID_WP='$idwp' ";
+        $wp_rsr[$j] = select_list($query_wp_rsr);
+    }
     
+    // Inizializzo la struttura progetti-matricole
+    for 
     
 } else {
     //==========================================================
