83b607caaca0abf7cc5f5739e5158faf3bc927d0
Alessandro <alessandrobarsanti6@gmail.com>
2021-02-26 14:23:52 +0100

    fix connect

--- ws/include/functions.php
+++ ws/include/functions.php
@@ -1,11 +1,9 @@
 <?php 
-
 use Firebase\JWT\JWT;
-
 
 function connect() {
   $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-  if (mysqli_connect_errno($connect)) {
+  if (mysqli_connect_errno()) {
     die("Failed to connect:" . mysqli_connect_error());
   }
   mysqli_set_charset($connect, "utf8");
