333e92c5034ffc0e564e6c0af1da30cd80862432
Alessandro <alessandrobarsanti6@gmail.com>
2021-02-26 10:40:13 +0100

    fix varie

--- ore-rd/src/app/_services/progetti.service.ts
+++ ore-rd/src/app/_services/progetti.service.ts
@@ -1,3 +1,4 @@
+import { Matricola } from './../_models/matricola';
 import { Progetto } from './../_models/progetto';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -9,23 +10,28 @@ export class ProgettiService {
     getAll() {
         return this.http.get<any>(environment.wsUrl+`Progetti.php`);
     }
+    
     getById(id_progetto: number) {
         return this.http.get<any>(environment.wsUrl+`Progetti.php?id_progetto=${id_progetto}`);
     }
+
     insert(progetto: Progetto) {
         return this.http.put<any>(environment.wsUrl+`Progetti.php`, progetto);
     }
+
     update(progetto: Progetto) {
         return this.http.post<any>(environment.wsUrl+`Progetti.php`, progetto);
     }
+
     delete(id_progetto: number) {
         return this.http.delete(environment.wsUrl+`Progetti.php?id_progetto=${id_progetto}`);
     }
 
     getAllTipiCostoPanthera() {
-        return '{"value":["TIPO1","TIPO2,"TIPO3"]}';
+        return this.http.get<any>(environment.wsUrl+`GetNomiUtenti.php`);;
     }
+
     getAllMatricole() {
-        return '{"value":[{"id_matricola":"1","nome":"A"},{"id_matricola":"2","nome":"B"}]}';
+        return this.http.get<any>(environment.wsUrl+`GetNomiUtenti.php`);;
     }
 }
