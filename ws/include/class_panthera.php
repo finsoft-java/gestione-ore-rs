6fdf26d2c7fa08e50aabd69ca15f172f147f3685
Luca Vercelli <luca.vercelli.to@gmail.com>
2021-02-26 10:43:22 +0100

    array

--- ws/include/class_panthera.php
+++ ws/include/class_panthera.php
@@ -5,26 +5,22 @@ $panthera = new PantheraManager();
 class PantheraManager {
 
     function getUtenti() {
-        $query = "SELECT DISTINCT MATRICOLA,NOME,COGNOME FROM THIP.DIPENDENTI_V01 WHERE ID_AZIENDA='001'";
+        $query = "SELECT DISTINCT MATRICOLA,COGNOME||' '||NOME AS NOME FROM THIP.DIPENDENTI_V01 WHERE ID_AZIENDA='001'";
         // TODO BISOGNA RICHIAMARE SQL SERVER, $matricole = select_list($query)
         
-        $matricole = [ [ 'MATRICOLA' => '1234', 'NOME' => 'Mario', 'COGNOME' => 'Rossi' ],
-                      [ 'MATRICOLA' => '4321', 'NOME' => 'Carlo', 'COGNOME' => 'Verdi' ],
-                      [ 'MATRICOLA' => '6666', 'NOME' => 'Gianni', 'COGNOME' => 'Bianchi' ]
+        $matricole = [ [ 'MATRICOLA' => '1234', 'NOME' => 'Rossi Mario' ],
+                      [ 'MATRICOLA' => '4321', 'NOME' => 'Verdi Carlo' ],
+                      [ 'MATRICOLA' => '6666', 'NOME' => 'Bianchi Gianni' ]
                      ];
-        $result = [];
-        foreach ($matricole as $m) {
-            $result[$m['MATRICOLA']] = $m['COGNOME'] . ' ' . $m['NOME'];
-        }
-        return $result;
+        return $matricole;
     }
 
     function getUtente($matricola) {
-        $query = "SELECT DISTINCT NOME,COGNOME FROM THIP.DIPENDENTI_V01 WHERE ID_AZIENDA='001' AND MATRICOLA='$matricola'";
+        $query = "SELECT DISTINCT COGNOME||' '||NOME AS NOME FROM THIP.DIPENDENTI_V01 WHERE ID_AZIENDA='001' AND MATRICOLA='$matricola'";
         // TODO BISOGNA RICHIAMARE SQL SERVER, $matricola = select_single($query)
         
-        $matricola = [ 'NOME' => 'Mario', 'COGNOME' => 'Rossi' ];
-        return "$nome $cognome";
+        $matricola = 'Rossi Mario';
+        return $matricola;
     }
 
     function getTipiCosto() {
@@ -35,11 +31,7 @@ class PantheraManager {
                       [ 'ID_TIPO_COSTO' => 'A02', 'DESCRIZIONE' => 'Prova 2' ],
                       [ 'ID_TIPO_COSTO' => 'A03', 'DESCRIZIONE' => 'Prova 3' ]
                      ];
-        $result = [];
-        foreach ($tipiCosto as $t) {
-            $result[$t['ID_TIPO_COSTO']] = $t['DESCRIZIONE'];
-        }
-        return $result;
+        return $tipiCosto;
     }
     
     function getCosti($data) {
@@ -54,11 +46,7 @@ class PantheraManager {
                       [ 'ID_RISORSA' => '4321', 'COSTO' => 9.0 ],
                       [ 'ID_RISORSA' => '6666', 'COSTO' => 10.0 ]
                      ];
-        $result = [];
-        foreach ($tipiCosto as $t) {
-            $result[$t['ID_RISORSA']] = $t['COSTO'];
-        }
-        return $result;
+        return $costi;
     }
 
 }
