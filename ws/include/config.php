87fd0cdfcdb80d8d22642ce7086a17a1567ded8c
Alessandro <alessandrobarsanti6@gmail.com>
2021-03-25 10:49:43 +0100

    fix per produzione

--- ws/include/config.php
+++ ws/include/config.php
@@ -6,11 +6,18 @@ define('DB_USER', 'user');
 define('DB_PASS', 'user');
 define('DB_NAME', 'questionari');
 */
-
+/* sviluppo
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'ore-rd');
+*/
+
+/* produzione */
+define('DB_HOST', 'localhost');
+define('DB_USER', 'rd');
+define('DB_PASS', 'Wn#5OHGt');
+define('DB_NAME', 'rd');
 
 define('JWT_SECRET_KEY', 'OSAISECRET2021');
 
@@ -27,6 +34,4 @@ define('DB_PTH_HOST', 'tcp:myserver.database.windows.net,1433');
 define('DB_PTH_USER', 'my_user');
 define('DB_PTH_PASS', 'my_pwd');
 define('DB_PTH_NAME', 'PANTH01');
-
-
 ?>
