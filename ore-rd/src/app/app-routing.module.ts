ce66dcb3374766aa4d5ce1445c058465abb2b70d
Luca Vercelli <luca.vercelli@it-present.com>
2021-10-28 10:56:30 +0200

    bugfix

--- ore-rd/src/app/app-routing.module.ts
+++ ore-rd/src/app/app-routing.module.ts
@@ -14,8 +14,8 @@ import { AuthGuard } from './_guards/auth.guard';
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'progetti', component: ProgettiComponent, canActivate:[AuthGuard]},
-  { path: 'progetto/nuovo', component: ProgettoDettaglioComponent, canActivate:[AuthGuard]},
   { path: 'progetto/:id_progetto', component: ProgettoDettaglioComponent, canActivate:[AuthGuard]},
+  { path: 'progetto/nuovo', component: ProgettoDettaglioComponent, canActivate:[AuthGuard]},
   { path: 'progetto/:id_progetto/report', component: ReportCompletoComponent, canActivate:[AuthGuard]},
   { path: 'tipologie-spesa', component: TipologiaSpesaComponent, canActivate:[AuthGuard]},
   { path: 'importazione-lul', component: ImportazioneLulComponent, canActivate:[AuthGuard]},
