fdea528648aed43528cebaac48caad5135af3296
Federica Pistillo <pistillo.federica@libero.it>
2022-10-21 15:21:10 +0200

    services upload commesse e partecipanti

---
+++ ore-rd/src/app/_services/upload.commesse.service .ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadCommesseService {
+
+  private baseUrl = environment.wsUrl;
+
+  constructor(private http: HttpClient) { }
+
+  upload(file: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+
+    const req = new HttpRequest('POST', `${this.baseUrl}/ImportExcelLul.php`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.http.request(req);
+  }
+  
+}
