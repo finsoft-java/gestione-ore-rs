14670d64b1d2c6c16168933544f47dc0c94f7448
gerjon.hodaj <gerjon.hodaj@it-present.com>
2022-10-25 10:19:08 +0200

    Inizio tabella commesse

---
+++ ore-rd/src/app/_services/commesse.service.ts
@@ -0,0 +1,16 @@
+import { Commessa, ListBean, ValueBean } from './../_models';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class CommesseService {
+
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+
+        return this.http.get<ListBean<Commessa>>(environment.wsUrl + `Commesse.php`);
+    }
+
+}
--- ore-rd/src/app/commesse/commesse.component.ts
+++ ore-rd/src/app/commesse/commesse.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Commessa } from '../_models';
+import { CommesseService } from '../_services/commesse.service';
 
 @Component({
   selector: 'app-commesse',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommesseComponent implements OnInit {
 
-  constructor() { }
+  dataSource = new MatTableDataSource<Commessa>();
+  displayedColumns: string[] = ['codCommessa', 'pctCompatibilita', 'totOrePreviste', 'totOreRdPreviste',
+    'tipologia', 'giustificativo'];
+
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  allCommesse: Array<any> = [];
+
+  constructor(
+    private commesseService: CommesseService) {
+  }
 
   ngOnInit(): void {
+
+    this.getAll();
+  }
+
+  getAll() {
+
+    this.commesseService.getAll().subscribe(response => {
+      this.allCommesse = response.data;
+      this.dataSource = new MatTableDataSource<Commessa>(response.data);
+    },
+      error => {
+      });
   }
 
 }
