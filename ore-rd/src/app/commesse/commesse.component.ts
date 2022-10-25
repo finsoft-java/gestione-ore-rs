d0fe6e9ac3da407cf01360093ae40fa3e299e78a
alessioMeddaPresent <alessio.medda@it-present.com>
2022-10-25 11:39:55 +0200

    Spostamento bottone upload a commesse

--- ore-rd/src/app/commesse/commesse.component.ts
+++ ore-rd/src/app/commesse/commesse.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Commessa } from '../_models';
+import { AlertService } from '../_services/alert.service';
+
 import { CommesseService } from '../_services/commesse.service';
 
 @Component({
@@ -18,7 +20,7 @@ export class CommesseComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
   allCommesse: Array<any> = [];
 
-  constructor(
+  constructor(private alertService: AlertService,
     private commesseService: CommesseService) {
   }
 
@@ -35,6 +37,53 @@ export class CommesseComponent implements OnInit {
     },
       error => {
       });
+  }
+
+  uploadGiustificativo(p: Commessa, event: any) {
+    console.log(event);
+    let file = event.target.files && event.target.files[0];
+    console.log('Going to upload:', file);
+    if (file) {
+      console.log(file);
+      this.commesseService.uploadGiustificativo(p.COD_COMMESSA!, file).subscribe(response => {
+        p.HAS_GIUSTIFICATIVO = 'Y';
+        p.GIUSTIFICATIVO_FILENAME = file.name;
+        this.alertService.success('Giustificativo caricato con successo');
+      },
+        error => {
+          this.alertService.error(error);
+        });
+    }
+  }
+
+  deleteGiustificativo(p: Commessa) {
+    // TODO Some warning?
+    this.commesseService.deleteGiustificativo(p.COD_COMMESSA!).subscribe(response => {
+      p.HAS_GIUSTIFICATIVO = 'N';
+      p.GIUSTIFICATIVO_FILENAME = null;
+      this.alertService.success('Giustificativo eliminato con successo');
+    },
+      error => {
+        this.alertService.error(error);
+      });
+  }
+
+  downloadGiustificativo(p: Commessa) {
+    this.commesseService.downloadGiustificativo(p.COD_COMMESSA!).subscribe(response => {
+      this.downloadFile(response, p.GIUSTIFICATIVO_FILENAME!);
+    },
+      error => {
+        this.alertService.error(error);
+      });
+  }
+
+  downloadFile(data: any, filename: string) {
+    const blob = new Blob([data] /* , { type: 'applicazion/zip' } */);
+    const url = window.URL.createObjectURL(blob);
+    var anchor = document.createElement("a");
+    anchor.download = filename;
+    anchor.href = url;
+    anchor.click();
   }
 
 }
